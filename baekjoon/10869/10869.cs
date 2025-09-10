4cd985fe69fdb51edfdf6a39c97e53528ef75dab
ruman kim <rumaniel@hotmail.com>
2025-09-10 18:57:04 +0900

    WIP test file based app

---
+++ baekjoon/10869/10869.cs
@@ -0,0 +1,17 @@
+Console.WriteLine($"parameter count = {args.Length}");
+
+if (args.Length > 0)
+{
+
+    for (int i = 0; i < args.Length; i++)
+    {
+        Console.WriteLine($"Arg[{i}] = [{args[i]}]");
+    }
+}
+else
+{
+    while (Console.ReadLine() is string line && line.Length > 0)
+    {
+        Console.WriteLine(line);
+    }
+}
